a0a484ded46e7bf4fd0e017a39960615b05aec52
kishalini11 <92731545+kishalini11@users.noreply.github.com>
2021-12-05 11:09:28 +0800

    versi awal

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Coach</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+    <div class="header">
+    <div class="navbar">
+        <div class="logo">
+           <a href="index.html"><img src="image/logocoach.png" width="125px"></a> 
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.html"></a>Home</li>
+                <li><a href="about.html"></a>About</li>
+                <li><a href=""></a>Add Cart</li>
+                <li><a href="booking.php"></a>booking</li>
+            </ul>
+        </nav>
+        <a href="cart.html"><img src="image/carta.png" width="30px" height="30px" width="30px" heigth="30px"></a>
+        </div>
+        <div class="row">
+            <div class="col-2">
+               <h1>The Secret To Having An Epically Beloved Bakery Is Consistency</h1>
+               <p>Baking may be regarded as a science, but it's the chemistry between the ingredients and the cook that gives desserts life. <br>Baking is done out of love, to share with family, to see them smile</p>
+               <a href="shop.php" class="btn">Shop Now &#8594;</a>
+            </div>
+            <div class="col-2">
+            <img src="image/coach.jpg">
+            </div>
+        </div>
+    </div>
+</div>
+
+<!---------------featured categories--------->
+       <div class="categories">
+           <div class="small-container">
+            <div class="row">
+                <div class="col-3">
+                    <img src="cake.jpeg">
+                </div>
+                <div class="col-3">
+                    <img src="cupcake5.jpeg">
+                </div>
+                <div class="col-3">
+                    <img src="brownies.jpeg">
+                </div>
+            </div>
+           </div>
+          
+       </div>
+<!---------------featured products--------->
+       <div class="small-comtainer">
+           <h2 class="title">Cakes</h2>
+           <div class="row">
+            <div class="col-4">
+                <img src="chocolate cakes.png">
+                <h4>Chocolate Cakes</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-half-o"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 45.00</p>
+            </div><div class="col-4">
+                <img src="vanilla cake.jpg">
+                <h4>Vanilla Cakes</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 35.00</p>
+            </div><div class="col-4">
+                <img src="strawberry cake.jpg">
+                <h4>Strawberry Cakes</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>RM 50.00</p>
+            </div><div class="col-4">
+                <img src="cheesecake.png">
+                <h4>Cheese Cakes</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 70.00</p>
+            </div>
+           </div>
+           <h2 class="title">Cupcakes</h2>
+           <div class="row">
+            <div class="col-4">
+                <img src="chocolate cupcakes.jpg">
+                <h4>Chocolate cupcakes</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-half-o"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 15.00</p>
+            </div><div class="col-4">
+                <img src="cupcake2.jpeg">
+                <h4>Lemon Swirl Cupcakes</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 17.00</p>
+            </div><div class="col-4">
+                <img src="cherry.jpg">
+                <h4>Cherry Coke Cupcakes</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>RM 20.00</p>
+            </div><div class="col-4">
+                <img src="almond.jpg">
+                <h4>Almond Joy Cupcakes</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 18.00</p>
+            </div>
+           </div>
+           <h2 class="title">Brownies</h2>
+           <div class="row">
+            <div class="col-4">
+                <img src="fudge-chocolate-brownies.jpg">
+                <h4>Fudgy Brownies</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-half-o"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 15.00</p>
+            </div><div class="col-4">
+                <img src="Chewy-Brownies-Thumbnail-scaled.jpg">
+                <h4>Chewy Brownies</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 17.00</p>
+            </div><div class="col-4">
+                <img src="cakery.jpg">
+                <h4>Cakery Brownies</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>RM 25.00</p>
+            </div><div class="col-4">
+                <img src="download.jpg">
+                <h4>Caramel Brownies</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 27.00</p>
+            </div>
+           </div>
+           <h2 class="title">Cookies</h2>
+           <div class="row">
+            <div class="col-4">
+                <img src="BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9-637x637-1.jpg">
+                <h4>Chocolate Chips Cookies</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-half-o"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 15.00</p>
+            </div><div class="col-4">
+                <img src="cookies.jpeg">
+                <h4>Almond Cookies</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 17.00</p>
+            </div><div class="col-4">
+                <img src="butter-cookies-4.jpg">
+                <h4>Butter Cookies</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-half-o"></i>
+                </div>
+                <p>RM 12.00</p>
+            </div><div class="col-4">
+                <img src="Cookies-and-cream-cookies-recipe-1-of-1.jpg">
+                <h4>Cookies Cream</h4>
+                <div class="rating">
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                 <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM 18.00</p>
+            </div>
+           </div>
+       </div>
+</body>
+</html>
